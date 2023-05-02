2b22cc339321b5203aa8932fba1aa9fe5eface26
Precious Akams <preciousakams@yahoo.com>
2023-05-02 10:51:13 +0100

    initial update

---
+++ src/item.js
@@ -0,0 +1,28 @@
+const sizes = ['tiny', 'small', 'medium', 'large', 'huge'];
+const colors = ['navy', 'blue', 'aqua', 'teal', 'olive', 'green', 'lime', 'yellow', 'orange', 'red', 'maroon', 'fuchsia', 'purple', 'silver', 'gray', 'black'];
+const fruits = ['apple', 'banana', 'watermelon', 'orange', 'peach', 'tangerine', 'pear', 'kiwi', 'mango', 'pineapple'];
+
+const Items = sizes.reduce(
+  (items, size) => [
+    ...items,
+    ...fruits.reduce(
+      (acc, fruit) => [
+        ...acc,
+        ...colors.reduce(
+          (acc, color) => [
+            ...acc,
+            {
+              name: `${size} ${color} ${fruit}`,
+              color,
+            },
+          ],
+          [],
+        ),
+      ],
+      [],
+    ),
+  ],
+  [],
+);
+
+export default Items;
